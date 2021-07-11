a5c3647deb01a39ce24e627d934c5195f7e8ebfa
jana <rathan.kparam@gmail.com>
2021-07-11 19:11:02 +0800

    Init config server

---
+++ sc-config-server/src/main/java/org/jana/scconfigserver/ScConfigServerApplication.java
@@ -0,0 +1,13 @@
+package org.jana.scconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScConfigServerApplication.class, args);
+	}
+
+}
