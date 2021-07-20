13ded13232c3563ba5adab31f1a162b0cf99fe34
jana <rathan.kparam@gmail.com>
2021-07-20 09:53:12 +0800

    Init currency-conversion-service

---
+++ currency-conversion-service/currency-conversion-service/src/test/java/org/jana/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jana.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
