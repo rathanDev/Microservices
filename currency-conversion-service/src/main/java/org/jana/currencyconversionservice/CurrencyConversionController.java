57217a1bee0ad6adb34ea65001bfc1d0b706bfa4
jana <rathan.kparam@gmail.com>
2021-07-20 10:26:06 +0800

    Init CurrencyConversionController

---
+++ currency-conversion-service/src/main/java/org/jana/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,19 @@
+package org.jana.currencyconversionservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from,
+                                                          @PathVariable String to,
+                                                          @PathVariable long quantity) {
+        return new CurrencyConversion(10001L, from, to, BigDecimal.ONE, BigDecimal.ONE, BigDecimal.ONE, "dev");
+    }
+
+}
