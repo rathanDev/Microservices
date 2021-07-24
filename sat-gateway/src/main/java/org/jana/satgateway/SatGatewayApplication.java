631faab043f55fa5a82150c8216c35f9b67e88db
jana <rathan.kparam@gmail.com>
2021-07-24 19:30:20 +0800

    Init API Gateway

---
+++ sat-gateway/src/main/java/org/jana/satgateway/SatGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.jana.satgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SatGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SatGatewayApplication.class, args);
+	}
+
+}
---
+++ sat-gateway/src/test/java/org/jana/satgateway/SatGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jana.satgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
