9a2c57d24a6225886c3a3edebb03a814b6c48256
jana <rathan.kparam@gmail.com>
2021-07-25 12:13:39 +0800

    Add retry and fallback method

--- currency-exchange-service/src/main/java/org/jana/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/org/jana/currencyexchangeservice/CircuitBreakerController.java
@@ -1,14 +1,29 @@
 package org.jana.currencyexchangeservice;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
+    // @Retry(name = "default") // thrice
+    @Retry(name = "sample-api", fallbackMethod = "getHardcodedResponse")
     public String sampleApi() {
-        return "Sample API";
+        logger.info("Sample api call received");
+        return new RestTemplate()
+                .getForEntity("http://locahost:8080/some-dummy-url", String.class)
+                .getBody();
+    }
+
+    public String getHardcodedResponse(Exception e) {
+        logger.info("Get hardcoded response");
+        return "hardcoded-response";
     }
 
 }
