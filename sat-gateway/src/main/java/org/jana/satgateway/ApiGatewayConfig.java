480549ce0f6fc0f8436a1802092be6379c8e5762
jana <rathan.kparam@gmail.com>
2021-07-25 10:13:14 +0800

    Add custom routes

---
+++ sat-gateway/src/main/java/org/jana/satgateway/ApiGatewayConfig.java
@@ -0,0 +1,41 @@
+package org.jana.satgateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+
+        // without customization
+        // return builder.routes().build();
+
+        /*
+        Function<PredicateSpec, Buildable<Route>> routeFunc
+                = p -> p
+                .path("/get")
+                .filters(f -> f
+                        .addRequestHeader("MyHeader", "MyURI")
+                        .addRequestParameter("MyParam", "MyValue"))
+                .uri("http://httpbin.org:80");
+        */
+
+        return builder
+                .routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f
+                                .addRequestHeader("MyHeader", "MyURI")
+                                .addRequestParameter("MyParam", "MyValue"))
+                        .uri("http://httpbin.org:80"))
+                .route(p -> p
+                        .path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .build();
+    }
+
+}
