e612f84312e0950cec7fa7e20830b4ed9e030613
jana <rathan.kparam@gmail.com>
2023-05-06 18:06:28 +0800

    Add API Gateway

---
+++ Gateway/cloud-gateway/src/main/java/org/jana/cloudgateway/CloudGatewayApp.java
@@ -0,0 +1,15 @@
+package org.jana.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudGatewayApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApp.class, args);
+	}
+
+}
